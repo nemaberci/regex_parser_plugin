f97093b97cdb9edb97d6935c62946d0a414a2991
bertalan.kovacs <bertalan.kovacs@ivm-vending.com>
2022-11-07 14:47:40 +0100

    Use annotation processor

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "hu.nemaberci"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 dependencies {
     implementation(kotlin("stdlib"))
--- src/main/kotlin/hu.nemaberci.CodeGenPlugin.gradle.kts
+++ src/main/kotlin/hu.nemaberci.CodeGenPlugin.gradle.kts
@@ -1,9 +1,6 @@
-import hu.nemaberci.generator.CodeGeneratorOrchestrator
-import java.nio.file.Files
-import kotlin.streams.toList
-
 plugins {
     `java-base`
+    `java-library`
     java
 }
 
@@ -13,40 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("hu.nemaberci:code-gen:1.0-SNAPSHOT")
+    annotationProcessor("hu.nemaberci:code-gen:1.0-SNAPSHOT")
+    api("hu.nemaberci:regex-api:1.0-SNAPSHOT")
 }
 
-sourceSets["main"].java.srcDir(file("build/generated/sources/regex"))
-
-task("generateRegex") {
-    val codeGenOrchestrator = CodeGeneratorOrchestrator()
-    val srcPath = file("src").toPath()
-    println(
-        Files.walk(srcPath)
-            .filter { !srcPath.relativize(it).startsWith("generated_sources") }
-            .filter { it.fileName.toString().endsWith(".java") }.toList()
-    )
-    sourceSets.forEach {
-        println(it)
-        println("qwe123")
-        it.java.srcDirs.forEach { srcDir ->
-            Files.walk(srcDir.toPath())
-                .filter { sourceFile -> sourceFile.fileName.toString().endsWith(".java") }
-                .forEach { sourceFile ->
-                    run {
-                        println(file("build/generated/sources/regex").toPath()
-                            .resolve(srcDir.toPath().relativize(sourceFile))
-                            .toFile())
-                        codeGenOrchestrator.generateParser(
-                            sourceFile.toFile(),
-                            file("build/generated/sources/regex")
-                        )
-                    }
-            }
-        }
-    }
-}
-
-tasks.withType<JavaCompile> {
-    dependsOn("generateRegex")
-}
